069af391d36d395347bd7199e00ed2a194500047
Roland Schenk <roland.schenk@hoermoos.ch>
2023-09-22 11:03:59 +0200

    integrate sss gmmaps component

---
+++ src/es/components/atoms/gMapsMarker/GMapsMarker.js
@@ -0,0 +1,33 @@
+﻿import { Shadow } from '../../web-components-toolbox/src/es/components/prototypes/Shadow.js';
+
+export default class GMapsMarker extends Shadow() {
+    constructor(...args) {
+        super(...args);
+    }
+
+
+
+
+    connectedCallback() {
+
+    }
+
+
+    disconnectedCallback() {
+
+    }
+
+    shouldComponentRenderHTML() {
+        return false;
+    }
+
+    shouldComponentRenderCSS() {
+        return false;
+    }
+
+
+
+    renderHTML() {
+
+    }
+};
