80087a847377c58b122b75992cd6160fcf9e6ad2
Roland Schenk <roland.schenk@hoermoos.ch>
2023-10-13 18:26:13 +0200

    meber form stuff

---
+++ src/es/components/organisms/apiAuthComponent/ApiAuthComponent.js
@@ -0,0 +1,99 @@
+﻿import { Shadow } from '../../web-components-toolbox/src/es/components/prototypes/Shadow.js';
+
+export default class ApiAuthComponent extends Shadow() {
+
+
+
+    constructor(...args) {
+        super(...args);
+        
+
+    }
+
+    connectedCallback() {
+        this._isLoggedIn = this.getAttribute("is-logged-in");
+        console.log(this._isLoggedIn);
+        this.render();
+    }
+
+
+
+    render() {
+        this.html = `
+      <form style="display: ${this._isLoggedIn ? 'none' : 'block'};" id="authForm">
+        <label for="username">Benutzername:</label><br>
+        <input type="text" id="username" name="username"><br>
+        <label for="password">Passwort:</label><br>
+        <input type="password" id="password" name="password"><br><br>
+        <input type="button" value="Absenden">
+      </form>
+      <div id="logoutDiv" style="display: ${this._isLoggedIn ? 'block' : 'none'};">
+        <button id="logoutButton">Logout</button>
+      </div>
+    `;
+
+        this.root.querySelector('input[type="button"]').addEventListener('click', () => this.login());
+        this.root.querySelector('#logoutButton').addEventListener('click', () => this.logout());
+        this.updateView();
+    }
+
+    updateView() {
+        const form = this.root.querySelector('#authForm');
+        const logoutDiv = this.root.querySelector('#logoutDiv');
+
+        console.log("update view");
+        if (this._isLoggedIn) {
+            form.style.display = 'none';
+            logoutDiv.style.display = 'block';
+        } else {
+            form.style.display = 'block';
+            logoutDiv.style.display = 'none';
+        }
+    }
+
+    async login() {
+        const username = this.root.querySelector('#username').value;
+        const password = this.root.querySelector('#password').value;
+
+        const response = await fetch('/api/authenticate', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username,
+                password
+            })
+        });
+
+        if (!response.ok) {
+            console.error('Authentifizierung fehlgeschlagen:', response.status);
+            return;
+        }
+
+        const data = await response.json();
+        localStorage.setItem('token', data.token);
+
+        // Verstecken Sie das Formular und zeigen Sie den Logout-Button an
+        this.root.querySelector('#authForm').style.display = 'none';
+        this.root.querySelector('#logoutDiv').style.display = 'block';
+        location.reload();
+    }
+
+    async logout() {
+        // Entfernen Sie das Token und zeigen Sie das Formular erneut an
+     
+        const response = await fetch('/api/logout', {
+            method: 'GET'
+        });
+        localStorage.removeItem('token');
+
+        if (!response.ok) {
+            console.error('Logout fehlgeschlagen:', response.status);
+            return;
+        }
+        this.root.querySelector('#authForm').style.display = 'block';
+        this.root.querySelector('#logoutDiv').style.display = 'none';
+        location.reload();
+    }
+}
---
+++ src/es/components/organisms/registerMemberForm/RegisterMemberForm.js
@@ -0,0 +1,85 @@
+﻿import { Shadow } from '../../web-components-toolbox/src/es/components/prototypes/Shadow.js';
+
+export default class RegisterMemberForm extends Shadow() {
+
+    constructor(...args) {
+        super(...args);
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        this.html = `
+      <form id="registerForm">
+        <label>
+          Name: <input type="text" id="lastName" name="lastName">
+        </label>
+        <label>
+          Vorname: <input type="text" id="firstName" name="firstName">
+        </label>
+        <label>
+          Email: <input type="email" id="email" name="email">
+        </label>
+        <label>
+          Passwort: <input type="password" id="password" name="password">
+        </label>
+        <label>
+          Passwort wiederholen: <input type="password" id="repeatPassword" name="repeatPassword">
+        </label>
+        <input type="submit" value="Absenden">
+      </form>
+      <div id="message"></div>
+    `;
+
+        this.root.querySelector('#registerForm').addEventListener('submit', this.submitForm.bind(this));
+    }
+
+    async submitForm(e) {
+        e.preventDefault();
+
+        const lastName = this.root.querySelector('#lastName').value;
+        const firstName = this.root.querySelector('#firstName').value;
+        const email = this.root.querySelector('#email').value;
+        const password = this.root.querySelector('#password').value;
+        const repeatPassword = this.root.querySelector('#repeatPassword').value;
+
+        if (password !== repeatPassword) {
+            this.showMessage('Passwörter stimmen nicht überein!', 'error');
+            return;
+        }
+
+        const data = {
+            email,
+            password,
+            firstName,
+            lastName
+        };
+
+        try {
+            const response = await fetch('/api/registerMember', {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+
+            if (!response.ok) {
+                throw new Error('Fehler beim Senden der Anforderung');
+            }
+
+            this.showMessage('Erfolgreich registriert!', 'success');
+        } catch (error) {
+            this.showMessage(error.message, 'error');
+        }
+    }
+
+    showMessage(message, type) {
+        const messageDiv = this.root.querySelector('#message');
+        messageDiv.textContent = message;
+        messageDiv.classList.add(type);
+    }
+}
+
