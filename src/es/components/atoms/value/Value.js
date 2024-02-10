5c924e51435cb5f0ae7037245d59076c76dce032
Weedshaker <weedshaker@gmail.com>
2024-02-10 12:23:34 +0100

    fixes sub

--- src/es/components/atoms/value/Value.js
+++ src/es/components/atoms/value/Value.js
@@ -25,6 +25,8 @@ export default class Value extends Shadow() {
   renderCSS () {
     this.css = `
             :host {
+                display: block;
+                margin-bottom: 1rem;
                 width: 100%;
             }
             
